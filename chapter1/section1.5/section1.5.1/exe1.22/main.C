10ddca47a5fc157dfa5ebe8789a8dd940453fc45
Ke Chen <ke7@ualberta.ca>
2019-04-08 16:30:10 -0700

    add code

--- chapter1/section1.5/section1.5.1/exe1.22/main.C
+++ chapter1/section1.5/section1.5.1/exe1.22/main.C
@@ -1,29 +1,39 @@
 #include <iostream>
 #include "Sales_item.h"
+
 int main()
 {
-    Sales_item item, item_sum;
+    Sales_item item, total;
     
-    std::cout << "Please enter a pair of transactions: 'ISBN' 'Number of copies sold' 'Sales price'" << std::endl;
+    std::cout << "Enter a transaction: 'ISBN' 'Number of copies sold' 'Sales price'"
+    << std::endl;
     
-    if (std::cin >> item)
+    if (std::cin >> total)
     {
-        item_sum = item;
-        std::cout << "Print the sum of the transactions:" << std::endl;
-        std::cout << item_sum << std::endl;
+        std::cout << "Enter next transaction: 'ISBN' 'Number of copies sold' 'Sales price'"
+        << std::endl;
         
-        std::cout << "Please enter a pair of transactions: 'ISBN' 'Number of copies sold' 'Sales price'" << std::endl;
         while (std::cin >> item)
         {
-            std::cout << "Please enter a transaction: 'ISBN' 'Number of copies sold' 'Sales price'" << std::endl;
-            item_sum += item;
-            std::cout << "Print the sum of the transactions:" << std::endl;
-            std::cout << item_sum << std::endl;
+            std::cout << "Enter next transaction: 'ISBN' 'Number of copies sold' 'Sales price'"
+            << std::endl;
+
+            if (total.isbn() == item.isbn())
+            {
+                total += item;
+            }
+            else
+            {
+                std::cerr << "Data must refer to the same ISBN" << std::endl;
+                return -1;
+            }
         }
+        std::cout << "The sum of the transactions:" << std::endl;
+        std::cout << total << std::endl;
     }
     else
     {
-        std::cout << "No input" << std::endl;
+        std::cout << "No data" << std::endl;
     }
     return 0;
 }
---
+++ chapter2/section2.6/section2.6.2/exe2.41_exe1.22/main.C
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main()
+{
+    Sales_data data, data_sum;
+    
+    double price = 0.0;
+
+    std::cout << "Input data: 'ISBN' 'Number of copies sold' 'Sales price'" << std::endl;
+    
+    if (std::cin >> data.bookNo >> data.units_sold >> price)
+    {
+        data.revenue = data.units_sold * price;
+        
+        data_sum = data;
+        
+        std::cout << "Input next data: 'ISBN' 'Number of copies sold' 'Sales price'" << std::endl;
+        
+        while (std::cin >> data.bookNo >> data.units_sold >> price)
+        {
+            
+            data.revenue = data.units_sold * price;
+            
+            if (data_sum.bookNo == data.bookNo)
+            {
+                data_sum.units_sold += data.units_sold;
+                
+                data_sum.revenue += data.revenue;
+                
+            }
+            else
+            {
+                std::cerr << "Data must refer to the same ISBN" << std::endl;
+                return -1;
+            }
+            
+            std::cout << "Input next data: 'ISBN' 'Number of copies sold' 'Sales price'" << std::endl;
+        }
+        
+        if (data_sum.units_sold != 0)
+        {
+            std::cout << "Average price is:" << std::endl;
+            
+            std::cout << data_sum.revenue/data_sum.units_sold << std::endl;
+        }
+        else
+            std::cout << "(no sales)" << std::endl;
+
+        std::cout << "The sum of objects is:" << std::endl;
+        
+        std::cout << data_sum.bookNo << " " << data_sum.units_sold << " " << data_sum.revenue << " " << std::endl;
+    }
+    else
+    {
+        std::cout << "No input" << std::endl;
+        return -1;
+    }
+    return 0;
+}
