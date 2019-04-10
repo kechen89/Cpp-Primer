a53408fb07c9a3c51b36bffc7f09861fff901a64
Ke Chen <ke7@ualberta.ca>
2019-04-10 16:11:04 -0700

    add code

---
+++ chapter3/section3.2.2/exe3.5a/main.C
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string str, str_con;
+    
+    while (cin >> str)
+    {
+        str_con += str;
+    }
+    
+    cout << str_con << endl;
+    
+    return 0;
+}
