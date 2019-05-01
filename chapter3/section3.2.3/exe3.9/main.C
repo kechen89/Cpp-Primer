0fba70adcfb1844f98c57232e240c0d88cb12903
Ke Chen <ke7@ualberta.ca>
2019-05-01 15:08:48 -0700

    add code

---
+++ chapter3/section3.2.3/exe3.9/main.C
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(){
+    string s;  //empty string
+    cout << s[0] << endl;  //s[0] is undefined
+}
