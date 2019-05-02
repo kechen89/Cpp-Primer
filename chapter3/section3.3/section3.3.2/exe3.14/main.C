fa125b65c1755af59655936c72ccf7c358c8329d
Ke Chen <ke7@ualberta.ca>
2019-05-02 10:52:56 -0700

    add code

---
+++ chapter3/section3.3/section3.3.2/exe3.14/main.C
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cin;
+using std::vector;
+
+int main(){
+
+    vector<int > ivec;
+    
+    int i;
+    
+    while(cin >> i) {
+        ivec.push_back(i);
+    }
+    
+}
