bea80f8e43c1abc05bcee928a401eab6bd1658f4
Ke Chen <ke7@ualberta.ca>
2019-05-07 14:12:14 -0700

    add code

---
+++ chapter3/section3.5/section3.5.3/exe3.35/main.C
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    int ia[] = {0,2,4,6,8};
+    
+    for (int *p = std::begin(ia); p != std::end(ia); ++p) {
+        *p = 0;
+        cout << *p << endl;
+    }
+    
+}
