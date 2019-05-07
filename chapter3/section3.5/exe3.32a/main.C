b2855d60a504eb455c9b796440770e7035f3a02a
Ke Chen <ke7@ualberta.ca>
2019-05-07 11:46:07 -0700

    add code

--- chapter3/section3.5/exe3.32a/main.C
+++ chapter3/section3.5/exe3.32a/main.C
@@ -10,9 +10,14 @@ using std::vector;
 int main(){
     constexpr size_t array_size = 10;
     int ia[array_size];
+    int ib[array_size];
     
-    for (size_t ix = 0; ix < array_size; ++ix) {  
+    for (size_t ix = 0; ix < array_size; ++ix)  
         ia[ix] = ix;
-        cout << ia[ix] << endl;
+    
+    for (size_t i = 0; i < array_size; ++i) {
+        ib[i] = ia[i];
+        cout << ib[i] << endl;
     }
+    
 }
