b1cadbae75a7d51b282a39051a016efe641713ae
Ke Chen <ke7@ualberta.ca>
2019-05-07 13:46:41 -0700

    add code

---
+++ chapter3/section3.5/section3.5.3/exe3.34/main.C
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    int ia[] = {0,2,4,6,8};
+    
+    int *p1 = ia;
+    
+    int *p2 = &ia[3];
+    
+    p1 += p2 - p1;
+    
+    cout << *p1 << endl;
+    
+}
