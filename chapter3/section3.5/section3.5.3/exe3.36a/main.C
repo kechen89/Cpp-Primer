b04fdb3ec334c9e65bbe6455083c37736c21f425
Ke Chen <ke7@ualberta.ca>
2019-05-07 14:43:20 -0700

    add code

---
+++ chapter3/section3.5/section3.5.3/exe3.36a/main.C
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    int ia[] = {0,2,4,6,8};
+    int ib[] = {0,2,4,6,8};
+    
+    auto na = std::end(ia) - std::begin(ia);
+    auto nb = std::end(ia) - std::begin(ia);
+    
+    int flag = 0;
+    
+    if (na == nb)
+    {
+        for (ptrdiff_t i = 0; i != na; ++i)
+        {
+            if (ia[i] != ib[i])
+                flag = 1;
+        }
+    }else
+    {
+        flag = 1;
+    }
+    
+    if (flag == 0)
+        cout << "The two arrays are equal" << endl;
+    else
+        cout << "The two arrays are not equal" << endl;
+    
+}
