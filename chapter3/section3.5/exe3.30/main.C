bcabd43e861edd3c89aeb49d3618c2cdafed67f8
Ke Chen <ke7@ualberta.ca>
2019-05-07 11:31:43 -0700

    add code

---
+++ chapter3/section3.5/exe3.30/main.C
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main(){
+    constexpr size_t array_size = 10;
+    int ia[array_size];
+    for (size_t ix = 1; ix <= array_size; ++ix)  // error, ./main Abort trap: 6
+        ia[ix] = ix;
+}
