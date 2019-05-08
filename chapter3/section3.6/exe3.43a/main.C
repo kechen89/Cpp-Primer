da478688273f24cc3e1276fd8520153584ff5f43
Ke Chen <ke7@ualberta.ca>
2019-05-08 14:43:40 -0700

    add code

---
+++ chapter3/section3.6/exe3.43a/main.C
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int ia[3][4] = {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}
+    };
+    
+    for (int (&i)[4] : ia)
+        for (int j : i)
+        cout << j << " ";
+    cout << endl;
+    
+    return 0;
+}
