412ae66b200131851177526f15ac9bb2fd7f58be
Ke Chen <ke7@ualberta.ca>
2019-05-09 15:32:52 -0700

    add code

---
+++ chapter4/section4.3/exe4.11/main.C
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    double a, b, c, d;
+    
+    cin >> a >> b >> c >> d;
+    
+    if (a > b && b > c && c > d){
+        // do something
+    }
+    
+    return 0;
+}
