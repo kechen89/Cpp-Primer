e2bb808cdff4d0f4ece0071b02e2e00f27cc84eb
Ke Chen <ke7@ualberta.ca>
2019-05-09 17:44:12 -0700

    add code

---
+++ chapter4/section4.9/exe4.29/main.C
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int x[10];
+    int *p = x;
+    
+    cout << sizeof(x)/sizeof(*x) << endl;
+    cout << sizeof(p)/sizeof(*p) << endl;
+    cout << sizeof(p) << endl;
+}
