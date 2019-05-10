550dc5a06eb2deaf1e51629bee215e66041df34b
Ke Chen <ke7@ualberta.ca>
2019-05-09 17:32:08 -0700

    add code

---
+++ chapter4/section4.9/exe4.28/main.C
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << sizeof(char) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+}
