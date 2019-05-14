f1f0cab8bb4f1da0fbaf1b93910fadf02adc5074
Ke Chen <ke7@ualberta.ca>
2019-05-14 12:07:28 -0700

    add code

---
+++ chapter5/section5.5/section5.5.1/exe5.20/main.C
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main ()
+{
+    bool flag = true;
+    string str, currStr;
+    
+    if (cin  >> str)
+    {
+        while (cin >> currStr) {
+            
+            if (currStr == str) {
+                cout << str << " occurred twice " << endl;
+                flag = false;
+                break;
+            }
+            else
+                str = currStr;
+        }
+    }
+    else {
+        cout << "No input" << endl;
+    }
+
+    if (flag) cout << "No word was repeated" << endl;
+    
+    return 0;
+}
