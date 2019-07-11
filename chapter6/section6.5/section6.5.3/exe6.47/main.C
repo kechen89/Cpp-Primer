2563be6145ba37a988186497ac9d8d4db41b4136
Ke Chen <ke7@ualberta.ca>
2019-07-10 17:20:10 -0700

    add code

---
+++ chapter6/section6.5/section6.5.3/exe6.47/main.C
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void recursive_print(const vector<int> &vi)
+{
+#ifndef NDEBUG
+    // print the size of the vector on each call
+    cout << "Size of vector is: " << vi.size() << endl;
+#endif
+    
+    if (vi.size() > 1)
+        recursive_print(vector<int>(vi.begin(), vi.end() - 1));
+    cout << vi[vi.size() - 1] << endl;
+}
+
+int main ()
+{
+    recursive_print({1,2,3,4,5});
+    return 0;
+}
+
