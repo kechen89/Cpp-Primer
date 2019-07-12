3d2f1ab5224f309af0189d65e164ffd66f8ef68d
Ke Chen <ke7@ualberta.ca>
2019-07-11 17:06:58 -0700

    add code

--- chapter7/section7.1/section7.1.2/exe7.3/Sales_data.C
+++ chapter7/section7.1/section7.1.2/exe7.3/Sales_data.C
@@ -1,31 +1,6 @@
 #include <iostream>
 #include <string>
-
-struct Sales_data
-{
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
-    
-    std::string isbn() const { return bookNo; }
-    Sales_data& combine(const Sales_data&);
-    double avg_price() const;
-};
-
-double Sales_data::avg_price() const
-{
-    if (units_sold)
-        return revenue/units_sold;
-    else
-        return 0;
-}
-
-Sales_data& Sales_data::combine(const Sales_data &rhs)
-{
-    units_sold += rhs.units_sold; // add the members of rhs into the members of "this" object
-    revenue += rhs.revenue;
-    return *this;      // return the object on which the function was called
-}
+#include "Sales_data.h"
 
 int main()
 {
