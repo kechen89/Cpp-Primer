ea0b4a5a69ff288c8ee86e3b06656b0c28a2c581
Ke Chen <ke7@ualberta.ca>
2019-07-15 15:12:37 -0700

    add code

---
+++ chapter7/section7.3/section7.3.1/exe7.23/Screen.h
@@ -0,0 +1,41 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+#include <iostream>
+
+class Screen
+{
+public:
+    typedef std::string::size_type pos;
+    Screen() = default;      // needed because Screen has another constructor
+    // cursor initialized to 0 by its in-class initializer
+    Screen(pos ht, pos wd, char c): height(ht), width(wd), contents(ht * wd, c) { }
+    
+    char get() const                 // get the character at the cursor
+    { return contest[cursor];}       // implicitly inline
+    
+    inline char get(pos r, pos c) const;  // explicitly inline
+    Screen &move(pos r, pos c);     // can be made inline later
+    
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+};
+
+inline Screen &Screen::move(pos r, pos c)   // we can specify inline on the definition
+{
+    pos row = r * width;    // compute the row location
+    cursor = row + c;       // move cursor to the column within that row
+    return *this;           // return this object as an lvalue
+}
+
+char Screen::get(pos r, pos c) const   // declared as inline in the class
+{
+    pos row = r * width;            // compute row location
+    return contents[row + c];       // return character at the given column
+}
+
+
+#endif
