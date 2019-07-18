892c8a4602f36c2a5036046b6ab7a00e871434bb
Ke Chen <ke7@ualberta.ca>
2019-07-17 18:17:38 -0700

    add code

--- chapter7/section7.5/section7.5.4/exe7.49_0/Sales_data.C
+++ chapter7/section7.5/section7.5.4/exe7.49_0/Sales_data.C
@@ -11,7 +11,7 @@ int main()
     std::string null_book = "9-999-99999-9";
     // constructs a temporary Sales_data object
     // with units_sold and revenue equal to 0 and bookNo equal to null_book
-    //item.combine(null_book);
+    item.combine(null_book);
     
     //item.combine("9-999-99999-9"); // error: reference to type 'const Sales_data' could not bind to an lvalue of type 'const char [14]'
     
