03622219f35268f468469f3d678ee769b8d6835e
Ke Chen <ke7@ualberta.ca>
2019-07-19 17:10:18 -0700

    add code

--- chapter8/section8.2/section8.2.2/exe8.7/main.C
+++ chapter8/section8.2/section8.2.2/exe8.7/main.C
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
     else
     {
         ifstream in(argv[1]);
-        ofstream out(argv[2], ofstream::app);
+        ofstream out(argv[2]);
         
         if (in >> total.bookNo >> total.units_sold >> price)
         {
