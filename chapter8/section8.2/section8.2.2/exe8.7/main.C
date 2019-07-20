4968508c6b862b9dc1413a61a0920c1aad08b335
Ke Chen <ke7@ualberta.ca>
2019-07-19 17:07:30 -0700

    add code

---
+++ chapter8/section8.2/section8.2.2/exe8.7/main.C
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main(int argc, char **argv)
+{
+    Sales_data total;
+    
+    double price = 0.0;
+    
+    if (argc == 1)
+    {
+        cerr << "No input file " << endl;
+        return -1;
+    }
+    else
+    {
+        ifstream in(argv[1]);
+        ofstream out(argv[2], ofstream::app);
+        
+        if (in >> total.bookNo >> total.units_sold >> price)
+        {
+            total.revenue = total.units_sold * price;
+            
+            Sales_data trans;
+            
+            while (in >> trans.bookNo >> trans.units_sold >> price)
+            {
+                trans.revenue = trans.units_sold * price;
+                
+                if (total.bookNo == trans.bookNo)
+                {
+                    total.units_sold += trans.units_sold;
+                    total.revenue += trans.revenue;
+                }
+                else
+                {
+                    out << "The sum of objects is:" << total.bookNo << " " << total.units_sold << " "
+                    << total.revenue << std::endl;
+                
+                    if (total.units_sold != 0)
+                    {
+                        out << "Average price is:" << std::endl;
+                    
+                        out << total.revenue/total.units_sold << std::endl;
+                    }
+                    
+                    total = trans;    // remember the new value
+                }
+            }
+            out << "The sum of objects is:" << total.bookNo << " " << total.units_sold << " "
+            << total.revenue << std::endl;
+        
+            if (total.units_sold != 0)
+            {
+                out << "Average price is:" << std::endl;
+            
+                out << total.revenue/total.units_sold << std::endl;
+            }
+            
+        }
+        else
+        {
+            cerr << "No input" << std::endl;
+            return -1;
+        }
+    }
+    return 0;
+}
