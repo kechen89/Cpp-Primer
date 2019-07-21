bc1e3783f6440757412657a6b63922a259b35ea5
Ke Chen <ke7@ualberta.ca>
2019-07-21 12:03:25 -0700

    add code

--- chapter8/section8.3/section8.3.1/exe8.11/main.C
+++ chapter8/section8.3/section8.3.1/exe8.11/main.C
@@ -24,16 +24,26 @@ int main(int argc, char **argv)
     else
     {
         ifstream fin(argv[1]);
+        
+        istringstream record;
+        
         // read the input a line at a time until cin hits end-of-file (or another error)
         while (getline(fin, line))
         {
-            PersonInfo info;     // create an object to hold this record's data
-            istringstream record(line);   // bind record to the line we just read
+            PersonInfo info;         // create an object to hold this record's data
+            
+            record.clear();          // Note, this clear function is neccessary to reset the eofbit
+            
+            record.str(line);        // bind record to the line we just read
+            
             record >> info.name;     // read the name
+            
             while (record >> word)     // read the phone numbers
                 info.phones.push_back(word);    // and store them
+            
             people.push_back(info);       // append this record to people
         }
+        
         for (auto per : people)
         {
             cout << per.name << " ";
