47655d346236507314f05c0a15cc2f92c4116601
Ke Chen <ke7@ualberta.ca>
2019-08-30 22:29:36 -0700

    add code

--- chapter2/section2.3/section2.3.2/exe2.21/main.C
+++ chapter2/section2.3/section2.3.2/exe2.21/main.C
@@ -2,7 +2,15 @@
 int main()
 {
     int i = 0;
-    int *ip = i;    //main.C:5:10: error: cannot initialize a variable of type 'int *' with an lvalue of type 'int'
-    std::cout<< *ip << std::endl;
+    
+    //(a)
+    //double *dp = &i; //main.C:7:13: error: cannot initialize a variable of type 'double *' with an rvalue of type 'int *'
+    
+    //(b)
+    //int *ip = i;    //main.C:5:10: error: cannot initialize a variable of type 'int *' with an lvalue of type 'int'
+    
+    //(c)
+    int *p = &i;      //legal
+    
     return 0;
 }
