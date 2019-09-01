7545a893d45016d83fba1dd0d9927daf56ac9efe
Ke Chen <ke7@ualberta.ca>
2019-09-01 16:21:33 -0700

    add code

--- chapter2/section2.4/section2.4.4/exe2.32/main.C
+++ chapter2/section2.4/section2.4.4/exe2.32/main.C
@@ -1,6 +1,13 @@
 #include <iostream>
 int main()
 {
-    int null = 0, *p = null; // error: cannot initialize a variable of type 'int *' with an lvalue of type 'int'
-
+    //int null = 0, *p = null; // error: cannot initialize a variable of type 'int *' with an lvalue of type 'int'
+    
+    //(1)
+    int null = 0, *p = 0;  //legal
+    
+    //(2)
+    //constexpr int null = 0;
+    //int *p = null;              // error: cannot initialize a variable of type 'int *' with an lvalue of type 'const int'
+    
 }
