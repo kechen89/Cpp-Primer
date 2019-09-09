4a8fc550fdee0b6bf7c34f3fec5dc0d865964650
Ke Chen <ke7@ualberta.ca>
2019-09-08 21:32:22 -0700

    modify code

--- chapter3/section3.4/section3.4.1/exe3.21/main.C
+++ chapter3/section3.4/section3.4.1/exe3.21/main.C
@@ -14,36 +14,36 @@ int main(){
     // (b)
     vector<int> v2(10);
     cout << v2.size() << endl;
-    for (auto it = v2.begin(); it != v2.end(); ++it)
+    for (auto it = v2.cbegin(); it != v2.cend(); ++it)
         cout << *it << endl;
     
     // (c)
     vector<int> v3(10,42);
     cout << v3.size() << endl;
-    for (auto it = v3.begin(); it != v3.end(); ++it)
+    for (auto it = v3.cbegin(); it != v3.cend(); ++it)
         cout << *it << endl;
     
     // (d)
     vector<int> v4{10};
     cout << v4.size() << endl;
-    for (auto it = v4.begin(); it != v4.end(); ++it)
+    for (auto it = v4.cbegin(); it != v4.cend(); ++it)
         cout << *it << endl;
     
     // (e)
     vector<int> v5{10, 42};
     cout << v5.size() << endl;
-    for (auto it = v5.begin(); it != v5.end(); ++it)
+    for (auto it = v5.cbegin(); it != v5.cend(); ++it)
         cout << *it << endl;
     
     // (f)
     vector<string> v6{10};
     cout << v6.size() << endl;
-    for (auto it = v6.begin(); it != v6.end(); ++it)
+    for (auto it = v6.cbegin(); it != v6.cend(); ++it)
         cout << *it << endl;
     
     // (g)
     vector<string> v7{10, "hi"};
     cout << v7.size() << endl;
-    for (auto it = v7.begin(); it != v7.end(); ++it)
+    for (auto it = v7.cbegin(); it != v7.cend(); ++it)
         cout << *it << endl;
 }
